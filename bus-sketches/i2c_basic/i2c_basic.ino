bc64de2afb8de0e3bb4b141e03487a42e91f86d1
Elias Santistevan <elias.santistevan@proton.me>
2025-07-30 12:55:41 -0600

    Adds some more stuff

--- bus-sketches/i2c_basic/i2c_basic.ino
+++ bus-sketches/i2c_basic/i2c_basic.ino
@@ -14,68 +14,8 @@
 
 const int oledAddrOpen = 0x3C; 
 const int oledAddrClosed = 0x3D; 
-const int rowStart = 0x00; 
-const int rowEnd = 0x3F; 
-const int colStart = 0x00; 
-const int colEnd = 0x1F; 
-const int mono = 0x01;
-int led = LED_BUILTIN;
+int reset = A0;
 
-enum commandList
-{
-  commandControlByte = 0x00,
-  anotherControlByte = 0xC0,
-  controlDataByteFollow = 0x80,
-  ramControlByte = 0x40,
-  //Two byte command - command, start, stop
-  commandRowStartEnd = 0x22,  
-  //Two byte command - command, start, stop
-  commandColStartEnd = 0x21,  
-  commandStartLine = 0xA2,
-  commandContrastControl = 0x81,
-  commandDisplayOn = 0xAF,  
-  commandDisplayOff = 0xAE,  
-  commandPanelID = 0xE1,  
-  commandDriverID = 0xE2, //BUSY+ON/OFF+0x60
-  commandGrayMono = 0xAC,
-  commandSetReMapDown = 0xA0, //default 
-  commandCommonOutScan = 0xC8, // scan COM[n-1] to COM0 default = 0xC0 
-  commandDisplayRotation = 0xA3,
-  commandDisableEntireDisplay = 0xA4,
-  commandEnableEntireDisplay = 0xA5,
-  commandNormalDisplay = 0xA6, //default 
-  commandMultiplexRatio = 0xA8,
-  commandDisplayOffset = 0xD3,
-  commandDisplayDivideRatio = 0xD5,
-  commandDischargeFront = 0x93,
-  commandDischargeBack = 0xD8,
-  commandPreCharge = 0xD9,
-  commandSEGpads = 0xDA,
-  commandVCOMDeselectLevel = 0xDB, 
-  commandExternalIREF = 0xAD,
-}; 
-
-enum manufactureSettings
-{
-  monoMode = 0x01, //grayscale by default "0x00"
-  manRowStart = 0x00, //default
-  manColStart = 0x00, //default
-  manRowEnd = 0x3F, //default
-  manColEnd = 0x1F, //default
-  manDisplayStart = 0x00, //default
-  horizontalAddressing = 0x00, //default 
-  contrast = 0xC8, //default = 0x80, upper end = 0xFF
-  rotateDisplayNinety = 0x01, //Default is 0 degrees
-  multiplexRatio = 0x7F, //default is 0x9F
-  displayOffset = 0x10, // default is 0x00
-  divideRatio = 0x00, //default
-  dischargeFront = 0x02,// default is 0x02
-  dischargeBack = 0x02,//default
-  preCharge = 0x1F,//0x1F is default
-  segPads = 0x00,//default 
-  vcomDeselect = 0x3F,//default 
-  externalIREF = 0x02
-};
 
 void setup()
 {
@@ -84,17 +24,13 @@ void setup()
   //Serial2.begin(115200, SERIAL_8N1); // Default config settings
   Wire.begin();
 
-  while(!Serial) // Circle the void until the monitor opens
-      ;
-
   Serial.println("I2C Sketch test.");
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-    
+  pinMode(reset, OUTPUT);
+  digitalWrite(reset, LOW); 
+  delay(10);
+  digitalWrite(reset, HIGH); 
+
   //Wire.setClock(10000); //Low mode
   //Wire.setClock(4000000); //Fast mode
   //Wire.setClock(1000000); //Fast mode plus
@@ -106,17 +42,28 @@ void setup()
 
   // Sanity Check.
   //getDriverID();
-  //manufactureSetup();
-  //fillSquare();
-  //writeCommand(commandDisplayOff);
+  manufactureSetup();
+  //emptySquare();
 
   Serial.println("Write finished, Looping.");
-  writeCommand(commandDisableEntireDisplay);
+  //writeCommand(commandDisableEntireDisplay);
   Serial.print("Checking if display is on: ");
   getDriverID();
 }
 
 void loop()
 {
-    delay(1000);
+  for( int row = 0; row < 0x3F; row++)
+  {
+    writeCommandParameter(0xB0, row);
+    writeCommandParameter(0x00, 0x00);
+    writeCommandParameter(0x10, 0x10);
+    delay(10);
+      for(int i = 0; i < 0x0F; i++)
+      {
+        fillSquare(i);
+        delay(100);
+      }
+    }
+  delay(100);
 }
